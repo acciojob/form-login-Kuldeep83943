e740876773bf366b632ef216fbb365c17bb5d426
Kuldeep83943 <134301330+Kuldeep83943@users.noreply.github.com>
2024-04-25 20:32:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
-    //Write your code here
+    //Write your code 
+	let form = document.getElementById("form1");
+	let fname = form.fname.value;
+	let lname = form.lname.value;
+	alert(fname,lname);
 
 }
